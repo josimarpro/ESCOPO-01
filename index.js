1637a2fb21fc96c74a094851daa67ebb1502a28d
josimar ferreira <josima-tamires@hotmail.com>
2024-07-04 14:46:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+if(true){
+const mensagem = 'ola'
+
+console. log(mensagem)
+}
+function falar(){
+    const mensagem = 'olá mundo!'
+    console.log(mensagem)
+    function falaroutracoisa(){
+
+    }
+    falaroutracoisa()
+    console.log(falarOutracoisa)
+}
